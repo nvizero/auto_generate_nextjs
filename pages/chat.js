33b0dfa6b4666786cf3e1b531df02999044b485f
victor.tseng <withvictor@gmail.com>
2023-12-05 18:15:53 +0800

    美化

--- pages/chat.js
+++ pages/chat.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 
 export default function Chat(props) {
-
   const [isConnected, setIsConnected] = useState(false);
   const [data, setData] = useState([]);
   const [ws, setWS] = useState(null);
@@ -32,11 +31,43 @@ export default function Chat(props) {
     };
   }, []); // Empty dependency array to run the effect only once on component mount
 
+  // 分割消息并创建列表项
+  const createListItems = (message) => {
+    const parts = message.split('\n').map((part, index) => {
+      return (
+        <li key={index} dangerouslySetInnerHTML={{ __html: convertAnsiToHtml(part) }}></li>
+      );
+    });
+    return <ul>{parts}</ul>;
+  };
+
+  function convertAnsiToHtml(text) {
+      // Define a mapping from ANSI color codes to CSS colors
+      const colorMap = {
+          '31': 'red',    // Red
+          '32': 'green',  // Green
+          '33': 'orange'  // Orange (closest to ANSI yellow)
+          // Add more mappings as needed
+      };
+
+      // Regular expression to match ANSI escape codes
+      const regex = /\u001b\[(\d+)m(.*?)\u001b\[39m/g;
+
+      return text.replace(regex, (match, p1, p2) => {
+          const color = colorMap[p1];
+          return color ? `<span style="color: ${color}">${p2}</span>` : p2;
+      });
+  }
+
+
   return (
     <div>
       {isConnected ? "Connected" : "Not Connected"}
-      <div>{ data.map((item, index) => <div key={index}>{item.reply}</div>) }</div>
+      <div>
+        {data.map((item, index) => (
+          <div key={index}>{createListItems(item.reply)}</div>
+        ))}
+      </div>
     </div>
   );
 }
-
